b4a63bb58ca7add7c867f2e325ef801360ed9533
alec.ptt <alec.ptt@eduge.ch>
2021-09-17 10:02:56 +0200

    mouvement des boutons dans le menu fonctinnel et bouton play lance la partie

--- BugsDestroyer/Menu.cs
+++ BugsDestroyer/Menu.cs
@@ -43,8 +43,6 @@ namespace BugsDestroyer
                 colorSectionPlayer = Color.LightGray * 0.7f;
                 colorSectionGame = Color.White;
                 
-
-
             }
             else if (!isSectionPlayer && (Keyboard.GetState().IsKeyDown(Keys.W) || Keyboard.GetState().IsKeyDown(Keys.Up)))
             {
@@ -56,15 +54,33 @@ namespace BugsDestroyer
 
             if (isSectionPlayer)
             {
-                if (selectedPlayer1 && (Keyboard.GetState().IsKeyDown(Keys.A) || Keyboard.GetState().IsKeyDown(Keys.Left) || Keyboard.GetState().IsKeyDown(Keys.D) || Keyboard.GetState().IsKeyDown(Keys.Right)))
+                if (selectedPlayer1 && (Keyboard.GetState().IsKeyDown(Keys.D) || Keyboard.GetState().IsKeyDown(Keys.Right)))
                 {
                     selectedPlayer1 = false;
                     selectedPlayerText = "2 players";
                 }
-                else if (!selectedPlayer1 && (Keyboard.GetState().IsKeyDown(Keys.A) || Keyboard.GetState().IsKeyDown(Keys.Left) || Keyboard.GetState().IsKeyDown(Keys.D) || Keyboard.GetState().IsKeyDown(Keys.Right)))
+                else if (!selectedPlayer1 && (Keyboard.GetState().IsKeyDown(Keys.A) || Keyboard.GetState().IsKeyDown(Keys.Left)))
                 {
                     selectedPlayer1 = true;
                     selectedPlayerText = "1 player";
+                }
+
+                //if (selectedPlayer1 && (Keyboard.GetState().IsKeyDown(Keys.A) || Keyboard.GetState().IsKeyDown(Keys.Left) || Keyboard.GetState().IsKeyDown(Keys.D) || Keyboard.GetState().IsKeyDown(Keys.Right)))
+                //{
+                //    selectedPlayer1 = false;
+                //    selectedPlayerText = "2 players";
+                //}
+                //else if (!selectedPlayer1 && (Keyboard.GetState().IsKeyDown(Keys.A) || Keyboard.GetState().IsKeyDown(Keys.Left) || Keyboard.GetState().IsKeyDown(Keys.D) || Keyboard.GetState().IsKeyDown(Keys.Right)))
+                //{
+                //    selectedPlayer1 = true;
+                //    selectedPlayerText = "1 player";
+                //}
+            }
+            else if (!isSectionPlayer)
+            {
+                if (isOnMenu && (Keyboard.GetState().IsKeyDown(Keys.D8)))
+                {
+                    isOnMenu = false;
                 }
             }
         }
@@ -78,9 +94,15 @@ namespace BugsDestroyer
             _spriteBatch.DrawString(font, "bugs destroyer", new Vector2(_graphics.PreferredBackBufferWidth / 2, 200), Color.White, 0f, new Vector2(font.MeasureString("bugs destroyer").X / 2, font.MeasureString("bugs destroyer").Y / 2), 2f, SpriteEffects.None, 0f);
 
             // Choix du joueur
-            _spriteBatch.Draw(_menuImages[2], new Vector2(_graphics.PreferredBackBufferWidth / 2 - 160, 430), null, colorSectionPlayer, 0f, Vector2.Zero, 2.75f, SpriteEffects.None, 0f);
+            if (!selectedPlayer1)
+            {
+                _spriteBatch.Draw(_menuImages[2], new Vector2(_graphics.PreferredBackBufferWidth / 2 - 175, 430), null, colorSectionPlayer, 0f, Vector2.Zero, 2.75f, SpriteEffects.None, 0f);
+            }
             _spriteBatch.DrawString(font, selectedPlayerText, new Vector2(_graphics.PreferredBackBufferWidth / 2, 450), colorSectionPlayer, 0f, new Vector2(font.MeasureString(selectedPlayerText).X / 2, font.MeasureString(selectedPlayerText).Y / 2), 0.75f, SpriteEffects.None, 0f);
-            _spriteBatch.Draw(_menuImages[1], new Vector2(_graphics.PreferredBackBufferWidth / 2 + 140, 430), null, colorSectionPlayer, 0f, Vector2.Zero, 2.75f, SpriteEffects.None, 0f);
+            if (selectedPlayer1)
+            {
+                _spriteBatch.Draw(_menuImages[1], new Vector2(_graphics.PreferredBackBufferWidth / 2 + 130, 430), null, colorSectionPlayer, 0f, Vector2.Zero, 2.75f, SpriteEffects.None, 0f);
+            }
 
             // Jeux
             _spriteBatch.DrawString(font, "Play", new Vector2(_graphics.PreferredBackBufferWidth / 2 - 50, 520), colorSectionGame, 0f, new Vector2(0, 0), 0.75f, SpriteEffects.None, 0f);
